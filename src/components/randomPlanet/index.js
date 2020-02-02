da754c80d8ce1d8c9df3f4be67f3431b1927f12d
VovaSidorov <VovaCivic@gmail.com>
2020-02-02 21:49:39 +0200

    get random planet from API

--- src/components/randomPlanet/index.js
+++ src/components/randomPlanet/index.js
@@ -1,33 +1,61 @@
 import React, { Component } from 'react';
 
-import SwapiService from '../../servises/swapi-servise';
+import SwapiService from "../../servises/swapi-servise";
 
 import './style.css';
 
 export default class RandomPlanet extends Component {
 
+    swapiService = new SwapiService();
 
+    state ={
+        id:null,
+        name:null,
+        population:null,
+        rotationPeriod:null,
+        diameter:null
+    };
 
+    constructor() {
+        super();
+        this.updatePlanet();
+    }
+
+    updatePlanet(){
+        const id = Math.floor(Math.random()*25)+2;
+        this.swapiService.getPlanet(id)
+            .then((planet)=>{
+                this.setState({
+                    id,
+                    name:planet.name,
+                    population:planet.population,
+                    rotationPeriod:planet.rotation_period,
+                    diameter:planet.diameter
+                })
+            });
+    }
     render() {
+
+        const {id, name, population,rotationPeriod,diameter} = this.state;
 
         return (
             <div className="random-planet jumbotron rounded">
                 <img className="planet-image"
-                     src={`https://starwars-visualguide.com/assets/img/planets/2.jpg`} />
+                     src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`} />
                 <div>
-                    <h4>Planet Name</h4>
+                    <h4>{name}</h4>
                     <ul className="list-group list-group-flush">
                         <li className="list-group-item">
                             <span className="term">Population</span>
-                            <span>1354</span>
+                            <span>{population}</span>
                         </li>
                         <li className="list-group-item">
                             <span className="term">Rotation Period</span>
-                            <span>5</span>
+                            <span>{rotationPeriod}</span>
                         </li>
                         <li className="list-group-item">
                             <span className="term">Diameter</span>
-                            <span>126</span>
+                            <span>{diameter}</span>
                         </li>
                     </ul>
                 </div>
--- src/servises/swapi-servise.js
+++ src/servises/swapi-servise.js
@@ -1,12 +1,13 @@
 
 export default class SwapiService{
-    _apiBase = 'https://swapi.co/api';
+    _apiBase = 'https://cors-anywhere.herokuapp.com/https://swapi.co/api';
 
     async getResourse (url){
         const res =await fetch(`${this._apiBase}${url}`);
 
         if (!res.ok) {
-            throw new Error(`Could not fetch ${url}`+`, received ${res.status}`)
+            throw new Error(`Could not fetch ${url}` +
+                `, received ${res.status}`)
         }
         return await res.json();
     };
