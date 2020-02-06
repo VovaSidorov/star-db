1dd6194e90418d733e62fbd79ee0aca2c1f29179
VovaSidorov <VovaCivic@gmail.com>
2020-02-06 23:29:25 +0200

    added data loading error handling

--- src/components/randomPlanet/index.js
+++ src/components/randomPlanet/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import SwapiService from "../../servises/swapi-servise";
 import Spinner from "../spinner";
+import ErrorIndicator from "../errorIndicator";
 
 import './style.css';
 
@@ -12,7 +13,8 @@ export default class RandomPlanet extends Component {
 
     state ={
         planet:{},
-        loading: true
+        loading: true,
+        error: false
     };
 
     constructor() {
@@ -25,22 +27,33 @@ export default class RandomPlanet extends Component {
                             loading:false})
     };
 
+    onError = (err)=>{
+         this.setState({
+             error:true,
+             loading:false
+         })
+    };
+
     updatePlanet(){
         const id = Math.floor(Math.random()*25)+2;
         this.swapiService
             .getPlanet(id)
-            .then(this.onPlanetLoaded);
+            .then(this.onPlanetLoaded)
+            .catch(this.onError);
     }
     render() {
 
-        const {planet,
-               loading} = this.state;
+        const {planet, loading, error} = this.state;
 
+        const hasData = !(loading||error);
+
+        const errorMessage = error ? <ErrorIndicator/>:null;
         const spinner = loading?<Spinner/>:null;
-        const content = !loading?<PlanetView planet={planet} />:null;
+        const content = hasData?<PlanetView planet={planet} />:null;
 
         return (
             <div className="random-planet jumbotron rounded">
+                {errorMessage}
                 {spinner}
                 {content}
             </div>
